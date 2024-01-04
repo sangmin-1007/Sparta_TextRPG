52f43020f0c5c228dd9b2c5b823221eab9fa32db
sangmin-1007 <dosin0822@gmail.com>
2024-01-04 10:45:16 +0900

    Player의 정보 출력하기
    정보 출력하기

---
+++ Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprata_TextRPG
+{
+
+    internal class Player
+    {
+        public int level;
+        public string name;
+        public string classType;
+        public int atk;
+        public int def;
+        public int hp;
+        public int gold;
+
+
+
+
+
+
+        public void CreatePlayer(int _level,string _name,  string _classType, int _atk, int _def, int _hp, int _gold)
+        {
+            level = _level;
+            name = _name;
+            classType = _classType;
+            atk = _atk;
+            def = _def;
+            hp = _hp;
+            gold = _gold;
+        }
+
+        public void Info()
+        {
+            string input = null;
+         
+            while(input != "0")
+            {
+                Console.WriteLine($"{name}  ({classType})");
+                Console.WriteLine($"공격력 : {atk}");
+                Console.WriteLine($"방어력 : {def}");
+                Console.WriteLine($"체력 : {hp}");
+                Console.WriteLine($"Gold : {gold}G");
+
+                Console.WriteLine();
+                Console.WriteLine("0.나가기");
+                Console.WriteLine();
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">>");
+                input = Console.ReadLine();
+
+                if(input !=  "0")
+                {
+                    Console.WriteLine("다시 입력해주세요");
+                }
+
+            }
+            Console.Clear();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,31 +4,21 @@
     {
         static bool run = true;
 
-        struct Player 
-        {
-            public int level;
-            public string name;
-            public string classType;
-            public int atk;
-            public int def;
-            public int hp;
-            public int gold;
-        }
-
 
         static void Main(string[] args)
         {
-            Player player;
-            CreatePlayer(out player);
+            Player player = new Player();
+            player.CreatePlayer(1,"김상민","전사",10,5,100,1500);
 
             while (run)
             {
-                StartScene(ref player);
+                StartScene(player);
             }
         }
 
-        static void StartScene(ref Player player)
+        static void StartScene(Player player)
         {
+            Console.Clear();
             Console.WriteLine("스파르타 마을에 오신것을 환영합니다");
             Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
 
@@ -44,7 +34,7 @@
             switch (input)
             {
                 case "1":
-                    Info(ref player);
+                    player.Info();
                     break;
                 case "2":
                     //
@@ -59,51 +49,8 @@
                     Console.WriteLine("잘못된 입력입니다. ");
                     Console.WriteLine(" ");
                     break;
-
-
             }
-        }
-
-        static void Info(ref Player player)
-        {
-            bool run = true;
-            while (run)
-            {
-                Console.WriteLine($"Lv . {player.level}");
-                Console.WriteLine($"{player.name} ({player.classType})");
-                Console.WriteLine($"공격력 : {player.atk}");
-                Console.WriteLine($"방어력 : {player.def}");
-                Console.WriteLine($"체 력 : {player.hp}");
-                Console.WriteLine($"Gold : {player.gold} G");
-                Console.WriteLine("");
-                Console.WriteLine("0. 나가기");
-                Console.Write(">>");
-                string input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "0":
-                        StartScene(ref player);
-                        run = false;
-                        break;
-                    default:
-                        Console.WriteLine("잘못 입력했습니다.");
-                        break;
-                }
-            }
-
-
-        }
-
-        static void CreatePlayer(out Player player)
-        {
-            player.level = 1;
-            player.name = "김상민";
-            player.classType = "전사";
-            player.atk = 10;
-            player.def = 5;
-            player.hp = 100;
-            player.gold = 1500;
+            
         }
     }
 }
